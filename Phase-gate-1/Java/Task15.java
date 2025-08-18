c79555fbe909b28f0a62bcc1b531db60a7246183
Mayorking14 <edwardmichael953@gmail.com>
2025-08-18 15:57:43 +0100

    Add files via upload

---
+++ Phase-gate-1/Java/Task15.java
@@ -0,0 +1,11 @@
+public class SumOfNaturalNumbers{
+public static void main(String... args){
+
+int sum = 0;
+for(int count = 1; count < 101; count++){
+sum += count;
+
+
+System.out.print(sum)
+
+}
